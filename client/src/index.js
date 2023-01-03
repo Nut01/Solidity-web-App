d67c810aa56af75938f7a5f83fcfbb596ca57ee6
Nut01 <99573711+Nut01@users.noreply.github.com>
2023-01-03 12:46:27 +0700

    Add First Page For Frontend It is Menu Page

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
-async function Testament() {
+
+
+async function testamentApp() {
     const web3 = await getWeb3();
     const accounts = await web3.eth.getAccounts();
     const contract = await getContract(web3);
 }
 
-Testament();
+testamentApp();
--- client/src/utils.js
+++ client/src/utils.js
@@ -3,7 +3,7 @@ const getWeb3 = () => {
     return new Promise((resolve, reject) => {
         window.addEventListener("load", async () => {
             try {
-                const web3 = new Web3("http://127.0.0.1:7545");
+                const web3 = new web3("http://127.0.0.1:7545");
                 resolve(web3);
             } catch (error) {
                 reject(error);
@@ -13,9 +13,9 @@ const getWeb3 = () => {
 }
 
 const getContract = async (web3) => {
-    const data = await $.getJSON("./contracts/Testament.json");
+    const data = await $.getJSON("./contracts/Greeting.json");
     const netID = await web3.eth.net.getId();
     const deployedNetwork = data.networkd[netID];
-    const testament = new web3.eth.Contract(data.abi, deployedNetwork && deployedNetwork.address);
-    return testament;
+    const greeting = new web3.eth.Contract(data.abi, deployedNetwork && deployedNetwork.address);
+    return greeting;
 }
