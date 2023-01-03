1e3615a2593ae21b18fbd65891e9cccb1a0ca081
Nut01 <99573711+Nut01@users.noreply.github.com>
2023-01-03 15:21:48 +0700

    return to use original ways

--- client/src/index.js
+++
@@ -1,9 +0,0 @@
-
-
-async function testamentApp() {
-    const web3 = await getWeb3();
-    const accounts = await web3.eth.getAccounts();
-    const contract = await getContract(web3);
-}
-
-testamentApp();
